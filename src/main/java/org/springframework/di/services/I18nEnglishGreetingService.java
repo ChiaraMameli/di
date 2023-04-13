4be18134e32cbda4df49c6c595d84a63420195f9
chiara.mameli <mameli.chiara@libero.it>
2023-04-13 12:13:53 +0200

    added default profile

--- src/main/java/org/springframework/di/services/I18nEnglishGreetingService.java
+++ src/main/java/org/springframework/di/services/I18nEnglishGreetingService.java
@@ -3,7 +3,7 @@ package org.springframework.di.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nGreetingService")
 public class I18nEnglishGreetingService implements GreetingService {
     @Override
